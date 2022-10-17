ee867f158deb31824a66520e00eb05711147fdde
Jhereck <dar55@hotmail.com>
2022-10-17 22:47:54 +0200

    First commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closed = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,11 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+closed.addEventListener("click", closeModal);
 
+// close modal form
+function closeModal() {
+  const modal = document.querySelector(".modal-body");
+  modalbg.style.display = "";
+}
